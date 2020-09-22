e828842c1a94eb9a8104ec52d28eba3038762b31
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-22 13:36:16 +0200

    styled-components implemented.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { unregister } from './core';
+import { GlobalStyles } from './styles'
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <GlobalStyles />
+        <div>Hello World!</div>
+    </>, document.getElementById('root'));
 
 unregister();
