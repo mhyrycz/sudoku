6f63d9cc40aaa9c207ae68a509a37d6ce2139442
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-23 13:55:58 +0200

    Grid lines added.

--- src/components/grid/styles/index.ts
+++ src/components/grid/styles/index.ts
@@ -1,11 +1,37 @@
-    import styled from 'styled-components'
+import styled, { css } from 'styled-components';
 
-    export const Container = styled.div`
-        display: flex;
-        flex-direction: column;
-    `
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
 
-    export const Row = styled.div`
-        display: flex;
-        flex-flow: row;
-    `
+export const Row = styled.div`
+	${({ theme }) => css`
+		display: flex;
+		flex-flow: row;
+
+		&:nth-child(1) {
+			border-top: solid 4px ${theme.colors.black};
+		}
+		&:nth-child(3),
+		&:nth-child(6) {
+			border-bottom: solid 3px ${theme.colors.black};
+		}
+		&:nth-child(9) {
+			border-bottom: solid 4px ${theme.colors.black};
+		}
+
+		div {
+			:nth-child(1) {
+				border-left: solid 5px ${theme.colors.black};
+			}
+			:nth-child(3),
+			:nth-child(6) {
+				border-right: solid 4px ${theme.colors.black};
+			}
+			:nth-child(9) {
+				border-right: solid 5px ${theme.colors.black};
+			}
+		}
+	`};
+`;
