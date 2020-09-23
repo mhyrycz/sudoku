373cf01a2c97ae0678b91d2756fa65d41ed63d8a
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-23 13:36:16 +0200

    Block styling created.

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,14 +1,14 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
+
+import { Container } from './styles';
 
 interface IProps {
-    colIndex: number
-    rowIndex: number
+	colIndex: number;
+	rowIndex: number;
 }
 
-const Block: FC<IProps> = ({colIndex, rowIndex}) => {
-    return (
-    <div data-cy="block">|{rowIndex} {colIndex}|</div>  
-    )
-}
+const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
+	return <Container data-cy={`block-${rowIndex}-${colIndex}`} />;
+};
 
 export default Block;
