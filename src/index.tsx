15db22f3561c87ddde160a159e19568be976c636
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-23 11:51:02 +0200

    title, card and contenent styled components created

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import { ThemeProvider } from 'styled-components';
 
 import { unregister } from './core';
 import { GlobalStyles, theme } from './styles';
+import { Card, Content, Title } from './components';
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
 		<GlobalStyles />
-		<div>Hello World</div>
+		{/* use can use data-cy to cypress e2e tests */}
+		<Content data-cy="content">
+			<Title data-cy="title">Sudoku</Title>
+			<Card data-cy="card">Work in progress</Card>
+		</Content>
 	</ThemeProvider>,
 	document.getElementById('root')
 );
