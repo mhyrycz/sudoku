269f5c94211ad57ec7ab60eefa58d33710c9fb17
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-24 09:02:28 +0200

    baseUrl changed. Grid types and utils folder added.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { unregister } from './core';
-import { GlobalStyles, theme } from './styles';
-import { Card, Content, Title, Grid} from './components';
+import { unregister } from 'core';
+import { GlobalStyles, theme } from 'styles';
+import { Card, Content, Title, Grid } from 'components';
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
@@ -12,7 +12,9 @@ ReactDOM.render(
 		{/* use can use data-cy to cypress e2e tests */}
 		<Content data-cy="content">
 			<Title data-cy="title">Sudoku</Title>
-			<Card data-cy="card"><Grid/></Card>
+			<Card data-cy="card">
+				<Grid />
+			</Card>
 		</Content>
 	</ThemeProvider>,
 	document.getElementById('root')
---
+++ src/typings/index.ts
@@ -0,0 +1,7 @@
+export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW];
+
+type NUMBERS = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+type N = 0 | NUMBERS;
+
+type ROW = [N, N, N, N, N, N, N, N, N];
