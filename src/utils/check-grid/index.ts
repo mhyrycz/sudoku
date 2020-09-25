675905491a39247fdcaa6cd28c2c83c67878ef87
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-25 13:13:51 +0200

    Checking whole grid implemented.

---
+++ src/utils/check-grid/index.ts
@@ -0,0 +1,8 @@
+import { GRID } from 'typings';
+
+function checkGrid(grid: GRID): boolean {
+	for (let i = 0; i < 9; i++) for (let j = 0; j < 9; j++) if (grid[i][j] === 0) return false;
+	return true;
+}
+
+export default checkGrid;
