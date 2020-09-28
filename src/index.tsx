a375cc88b399c78dce9f0d3cad7672139ddbf831
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-28 09:43:32 +0200

    Redux configured.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
-import { unregister } from 'core';
+import { configureStore, unregister } from 'core';
 import { GlobalStyles, theme } from 'styles';
 import { Card, Content, Title, Grid } from 'components';
+
+const store = configureStore();
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
 		<GlobalStyles />
 		{/* use can use data-cy to cypress e2e tests */}
-		<Content data-cy="content">
-			<Title data-cy="title">Sudoku</Title>
-			<Card data-cy="card">
-				<Grid />
-			</Card>
-		</Content>
+		<Provider store={store}>
+			<Content data-cy="content">
+				<Title data-cy="title">Sudoku</Title>
+				<Card data-cy="card">
+					<Grid />
+				</Card>
+			</Content>
+		</Provider>
 	</ThemeProvider>,
 	document.getElementById('root')
 );
