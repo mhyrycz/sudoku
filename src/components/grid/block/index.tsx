79d8c975d8261bcf46038a84c5d51e2333b19e1a
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-28 11:29:12 +0200

    Rendering grid blocks values.

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,14 +1,26 @@
 import React, { FC } from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { Container } from './styles';
+
+import { IReducer } from 'reducers';
+
+import { N } from 'typings';
 
 interface IProps {
 	colIndex: number;
 	rowIndex: number;
 }
 
+interface IState {
+	value: N;
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-	return <Container data-cy={`block-${rowIndex}-${colIndex}`} />;
+	const state = useSelector<IReducer, IState>(({ grid }) => ({ value: grid ? grid[colIndex][rowIndex] : 0 }));
+
+	return <Container data-cy={`block-${rowIndex}-${colIndex}`}>{state.value === 0 ? '' : state.value}</Container>;
 };
 
 export default Block;
