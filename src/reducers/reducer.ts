d654abf7fe20468e833a1b9866ded8b4613ebc3f
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-28 10:13:11 +0200

    CreateGrid action created.

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,9 +1,19 @@
 import { AnyAction } from 'redux';
 
-const initialState = {};
+import { createFullGrid } from 'utils';
+
+import { IReducer } from './interfaces';
+import * as types from './types';
+
+const initialState: IReducer = {};
 
 function reducer(state = initialState, action: AnyAction) {
 	switch (action.type) {
+		case types.CREATE_GRID:
+			return {
+				...state,
+				grid: createFullGrid()
+			};
 		default:
 			return state;
 	}
