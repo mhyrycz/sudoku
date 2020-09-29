e9d3e17fb27e5064c6f84225448ffeeefd7f6aae
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-29 10:39:19 +0200

    Copy grid.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,4 @@ export { default as identifySquare } from './identify-square';
 export { default as checkGrid } from './check-grid';
 export { default as createFullGrid } from './create-full-grid';
 export { default as getRandomIndex } from './get-random-index';
+export { default as copyGrid } from './copy-grid';
