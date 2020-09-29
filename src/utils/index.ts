428f0e6124142c9b714bcb4bbf23711a916756d2
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-09-29 10:18:30 +0200

    Remove numbers.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,4 @@ export * from './is-in';
 export { default as identifySquare } from './identify-square';
 export { default as checkGrid } from './check-grid';
 export { default as createFullGrid } from './create-full-grid';
+export { default as getRandomIndex } from './get-random-index';
